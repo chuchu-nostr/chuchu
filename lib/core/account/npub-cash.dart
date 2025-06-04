93775e266b51e208439d94abd8068807bbe17db3
nsnjx <90442692+nsnjx@users.noreply.github.com>
2025-06-04 18:18:48 +0800

    feat: 1. add account

---
+++ lib/core/account/npub-cash.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../nostr_dart/nostr.dart';
+import '../utils/log_utils.dart';
+import 'account.dart';
+
+class NpubCash {
+  static String address(String pubkey) {
+    return '${Nip19.encodePubkey(pubkey)}@npub.cash';
+  }
+
+  static Future<int?> balance() async {
+    String url = 'https://npub.cash/api/v1/balance';
+    String authToken = await Nip98.base64Event(
+        url, Account.sharedInstance.currentPubkey, Account.sharedInstance.currentPrivkey);
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Nostr $authToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var body = json.decode(response.body);
+      if (body['error'] == false) {
+        var data = body['data'];
+        if (data != null) {
+          if (data is String) {
+            return int.tryParse(data) ?? 0;
+          } else if (data is int) {
+            return data;
+          }
+        }
+      }
+    } else {
+      LogUtils.v(() => 'Request failed with status: ${response.statusCode}');
+    }
+    return null;
+  }
+
+  static Future<String?> claim() async {
+    String url = 'https://npub.cash/api/v1/claim';
+    String authToken = await Nip98.base64Event(
+        url, Account.sharedInstance.currentPubkey, Account.sharedInstance.currentPrivkey);
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Nostr $authToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var body = json.decode(response.body);
+      if (body['error'] == false) {
+        var data = body['data'];
+        var token = data['token'];
+        return token;
+      }
+    } else {
+      LogUtils.v(() => 'Request failed with status: ${response.statusCode}');
+    }
+    return null;
+  }
+}
