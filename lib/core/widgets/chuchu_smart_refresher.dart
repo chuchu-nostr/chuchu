8af71e6d53566fd73c5bea1b85e2c8819776f165
nsnjx <90442692+nsnjx@users.noreply.github.com>
2025-06-16 15:36:57 +0800

    feat: Add load more controls

---
+++ lib/core/widgets/chuchu_smart_refresher.dart
@@ -0,0 +1,196 @@
+
+import 'package:chuchu/core/utils/adapt.dart';
+import 'package:flutter/material.dart' hide RefreshIndicator, RefreshIndicatorState;
+import 'package:lottie/lottie.dart';
+
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import '../manager/cache/chuchu_cache_manager.dart';
+import '../utils/feed_utils.dart';
+
+export 'package:pull_to_refresh/src/smart_refresher.dart';
+
+class ChuChuSmartRefresher extends StatelessWidget {
+
+  ChuChuSmartRefresher({
+    Key? key,
+    required this.controller,
+    this.child,
+    this.header,
+    this.footer,
+    this.enablePullDown,
+    this.enablePullUp,
+    this.enableTwoLevel,
+    this.onRefresh,
+    this.onLoading,
+    this.onTwoLevel,
+    this.scrollController,
+  }) : super(key: key);
+
+  final RefreshController controller;
+  final Widget? child;
+  final Widget? header;
+  final Widget? footer;
+  final bool? enablePullDown;
+  final bool? enablePullUp;
+  final bool? enableTwoLevel;
+  final VoidCallback? onRefresh;
+  final VoidCallback? onLoading;
+  final OnTwoLevel? onTwoLevel;
+  final ScrollController? scrollController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SmartRefresher(
+        scrollController:scrollController,
+        controller: this.controller,
+        header: this.header ?? refresherHeader,
+        footer: this.footer ?? refresherFooter,
+        enablePullDown: this.enablePullDown ?? true,
+        enablePullUp: this.enablePullUp ?? false,
+        enableTwoLevel: this.enableTwoLevel ?? false,
+        onRefresh: this.onRefresh,
+        onLoading: this.onLoading,
+        onTwoLevel: this.onTwoLevel,
+        child: this.child
+    );
+  }
+
+  Widget get refresherHeader {
+    return LoadingHeader();
+  }
+
+  Widget get refresherFooter {
+    return ClassicFooter(
+        idleText: 'Pull up to load more',
+        loadingText: 'Loading',
+        noDataText: "-------- It's all down to the bottom --------",
+        failedText: "Failure",
+        canLoadingText: "Release and load",
+    );
+  }
+
+}
+
+class LoadingHeader extends RefreshIndicator {
+
+  @override
+  State<StatefulWidget> createState() {
+
+    // TODO: implement createState
+    return LoadingHeaderState();
+  }
+}
+
+class LoadingHeaderState extends RefreshIndicatorState<LoadingHeader>
+    with SingleTickerProviderStateMixin {
+
+  late final AnimationController _controller;
+
+  int? refreshTime;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // init frame is 2
+    super.initState();
+    _controller = AnimationController(vsync: this,value: 0);
+    _getUpdateTime();
+
+  }
+
+  @override
+  Future<void> endRefresh() {
+    // TODO: implement endRefresh
+    _controller.value = 1;
+    _controller.stop();
+    return super.endRefresh();
+  }
+
+  @override
+  void onModeChange(RefreshStatus? mode) {
+    // TODO: implement onModeChange
+    if (mode == RefreshStatus.canRefresh) {
+      _controller.repeat();
+    }else if(mode == RefreshStatus.completed){
+      ChuChuCacheManager.defaultOXCacheManager.saveData("pull_refresh_time", DateTime.now().millisecondsSinceEpoch);
+      setState(() {
+        refreshTime = DateTime.now().millisecondsSinceEpoch;
+      });
+
+    }
+    super.onModeChange(mode);
+  }
+
+  _getUpdateTime(){
+
+    ChuChuCacheManager.defaultOXCacheManager.getData("pull_refresh_time",defaultValue: null).then((value){
+
+      if(value != null){
+
+        setState(() {
+          refreshTime = value;
+        });
+      }
+    });
+
+  }
+
+
+  @override
+  void resetValue() {
+    // TODO: implement resetValue
+    super.resetValue();
+  }
+
+  @override
+  Widget buildContent(BuildContext context, RefreshStatus mode) {
+    // TODO: implement buildContent
+    return Column(
+      children: [
+        Lottie.asset(
+          "assets/chuchu_pull_loading.json",
+          package: 'ox_common',
+          width: 72.px,
+          repeat: true,
+          fit: BoxFit.fitWidth,
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller..duration = Duration(milliseconds: 1500);
+          },
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 4.px),
+          child: Text(_getRefreshTimeString(),style: TextStyle(
+              color: Colors.black,
+              fontSize: 12.px,
+          ),),
+        )
+      ],
+    );
+  }
+
+
+  _getRefreshTimeString(){
+
+    if(refreshTime != null){
+      String lastFresh = 'Last updated:';
+      if(DateUtils.isSameDay(DateTime.fromMillisecondsSinceEpoch(refreshTime!), DateTime.now())){
+        lastFresh = lastFresh + 'Today' + FeedUtils.formatTimestamp(refreshTime!);
+      }else{
+        lastFresh = lastFresh + FeedUtils.formatTimestamp(refreshTime!);
+      }
+      return lastFresh;
+    }
+
+
+    return "";
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
